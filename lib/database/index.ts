5eeaef35ab92a86a0eea9836920082bd879b3a11
Ayush Pachpandey <ayushpachpandey09@gmail.com>
2024-07-08 03:14:14 +0530

    change in database uri

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -1,23 +1,53 @@
-import mongoose from 'mongoose'
+// import mongoose from 'mongoose'
 
-const MONGODB_URI = process.env.MONGODB_URI
+// const MONGODB_URI = process.env.MONGODB_URI
 
-let cached = (global as any).mongoose || {conn: null, promise: null}
+// let cached = (global as any).mongoose || {conn: null, promise: null}
+
+// export const connectToDatabase = async () => {
+//     if(cached.conn) return cached.conn;
+
+//     if(!MONGODB_URI) throw new Error('MONGODB_URI is missing')
+    
+//     cached.promise = cached.promise || mongoose.connect(MONGODB_URI, {
+//         dbName: 'evently',
+//         bufferCommands: false
+//     })
+
+//     cached.conn = await cached.promise;
+
+//     return cached.conn
+// }
+import mongoose from 'mongoose';
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+if (!MONGODB_URI) {
+  throw new Error('MONGODB_URI is missing');
+}
+
+let cached = (global as any).mongoose;
+
+if (!cached) {
+  cached = (global as any).mongoose = { conn: null, promise: null };
+}
 
 export const connectToDatabase = async () => {
-    if(cached.conn) return cached.conn;
+  if (cached.conn) {
+    return cached.conn;
+  }
 
-    if(!MONGODB_URI) throw new Error('MONGODB_URI is missing')
-    
-    cached.promise = cached.promise || mongoose.connect(MONGODB_URI, {
-        dbName: 'evently',
-        bufferCommands: false
-    })
-
-    cached.conn = await cached.promise;
-
-    return cached.conn
-}
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URI, {
+      dbName: 'evently',
+      bufferCommands: false,
+    }).then((mongoose) => {
+      return mongoose;
+    });
+  }
+  cached.conn = await cached.promise;
+  return cached.conn;
+};
 
 // this is a way of connecting mongodb to next, this is
 // basically there are server actions and each server action
