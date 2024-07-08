46f7fefd1726942406c17bd541a394c31e1ed544
Ayush Pachpandey <ayushpachpandey09@gmail.com>
2024-07-09 00:46:34 +0530

    applied authmiddleware

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,18 @@
-import { clerkMiddleware } from '@clerk/nextjs/server';
+// import { clerkMiddleware } from '@clerk/nextjs/server';
 
-// Make sure that the `/api/webhooks/(.*)` route is not protected here
-export default clerkMiddleware()
+// // Make sure that the `/api/webhooks/(.*)` route is not protected here
+// export default clerkMiddleware()
+
+// export const config = {
+//   matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)', '/api/webhook/clerk'],
+// };
+
+import { authMiddleware } from "@clerk/nextjs/server";
+
+export default authMiddleware({
+    publicRoutes: ["/", "/api/webhook/clerk"],
+  ignoredRoutes: ["/api/webhook/clerk"]
+});
 
 export const config = {
   matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
